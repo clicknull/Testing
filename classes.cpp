e018093a7e8f7f7d9a9908ef20ec7c015f58e502
Matt Howard <pasvninja@gmail.com>
2011-06-19 15:16:26 -0700

    added classes.cpp

---
+++ classes.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+class parent_class {
+	public:
+		int i;
+		void parent_class(void) {
+			int t=0;
+			i=31337;
+			cout << "We are constructing the parent_class" << endl;
+		}
+		void ~parent_class(void) {
+			cout << "We are destructing the parent_class" << endl;
+		}
+	private:
+		void private_func(void) {
+			cout << "private_func" << endl;
+		}
+};
+
+class child_class : parent_class {
+	public:
+
+};
+
+
+int main(int argc, char **argv) {
+	parent_class test1;
+	child_class test2;
+	cout << "test2.i = " << test2.i << endl;
+}
