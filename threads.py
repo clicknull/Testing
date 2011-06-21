0123b19aab260da95a1e8903cc41784535137919
Matt Howard <pasvninja@gmail.com>
2011-06-21 14:50:35 -0700

    updated threads

--- threads.py
+++ threads.py
@@ -3,19 +3,18 @@ import thread as _thread, time
 
 mutex=_thread.allocate_lock()
 
-def count(_id,num):
-	
+def count(_id):
+	global num
 	while (num < 10):
 		mutex.acquire()
 		num += 1
 		print str(_id) + ": " + str(num)
 		mutex.release()
-		time.sleep(1)
+		#time.sleep(1)
 if(__name__ == "__main__"):
-	global num
 	num = 0	
 	mutex=_thread.allocate_lock()
 	for i in range (5):
-		_thread.start_new_thread(count, (i,num))
+		_thread.start_new_thread(count, (i,))
 	time.sleep(5) # not using join() just waiting for the other threads
 	print "main thread exiting..."
