b8e2b9204f24f9444621d3d9531f0cc01eab0999
m <pasvninja@gmail.com>
2012-07-07 18:53:17 -0700

    Coming together slowly, sloth-like even

--- test_regfunchooks.py
+++ test_regfunchooks.py
@@ -6,38 +6,85 @@ import pydbg
 import os,sys
 import utils
 
-# gimmes: -Function-         -DLL- -ArgMap- -callback- -func name-
 gimme=[]
-gimme.append({'dll' : 'advapi32.dll', 'argmap': 'isddi', 'hook': 'hook_Reg', 'func' :'RegCreateKeyExA'})
+
+HKEY_CLASSES_ROOT =		'0x80000000L'
+HKEY_CURRENT_USER =		'0x80000001L'
+HKEY_LOCAL_MACHINE =	'0x80000002L'
+HKEY_USERS =			'0x80000003L'
+
+hkeys={}
+hkeys[HKEY_CLASSES_ROOT] = "HKEY_CLASSES_ROOT"
+hkeys[HKEY_CURRENT_USER] = "HKEY_CURRENT_USER"
+hkeys[HKEY_LOCAL_MACHINE] = "HKEY_LOCAL_MACHINE"
+hkeys[HKEY_USERS] = "HKEY_USERS"
 
 pyd = pydbg.pydbg()
 hooks = utils.hook_container()
 
 # KISS for now... 
-def hook_RegCreateKeyExA(pyd, args):
+def read_arg_str(pyd, args, arg_num):
+	arg_num -= 1
 	buffer = ""
 	offset = 0
+
+	if args == 0: # NULL case
+		return 0
 	while 1:
-		byte = pyd.read_process_memory(args[1]+offset, 1)
+		byte = pyd.read_process_memory(args[arg_num]+offset, 1)
 		if(byte != "\x00"):
 			buffer += byte
 			offset += 1
 			continue
 		else:
 			break
-	
-	print "func(\""+buffer+"\")"
-		
+	if(len(buffer) > 0):
+		return buffer
+	else:
+		return -1
+
+
+# 64 bit option? # for reading size values from pointers..
+def read_arg_int(pyd, args, arg_num):
+	arg_num -= 1
+	the_int = int(pyd.read_process_memory(args[arg_num], 4))
+	return the_int
+
+# call backs
+def hook_RegCreateKeyExA(pyd, args):
+	buf=read_arg_str(pyd, args, 2)
+	print "RegCreateKeyExA(\""+buf+"\")"
+
+def hook_RegQueryValueExA(pyd, args):
+	buf=read_arg_str(pyd, args, 2)
+	print "RegQueryValueExA(\""+buf+"\")"
+
+def hook_RegGetValueA(pyd, args):
+	buf=read_arg_str(pyd, args, 2)
+	print "RegGetValueA(\""+buf+"\")"
+
+def hook_RegOpenKeyExA(pyd, args):
+	buf=read_arg_str(pyd, args, 2)
+	try:
+		key=hkeys[hex(args[0])]
+	except:
+		key=str(hex(args[0]))
+	print "RegOpenKeyExA("+key+", \""+buf+"\")"
+
 
 def hookme():
-	hook_addrs = None
-	hook_addr = pyd.func_resolve_debuggee(gimme[0]['dll'], gimme[0]['func'])
-	if not hook_addr:
-		print "[-] couldn't hook address of "+gimme[0]['func']+" in "+gimme[0]['dll']
-		sys.exit(-1)
-	print "[+] hooked "+gimme[0]['func']+"('"+gimme[0]['argmap']+"') in "+gimme[0]['dll'] + " at 0x%08x !" % hook_addr
-	hooks.add(pyd, hook_addr, 9, hook_RegCreateKeyExA, None) # change me later..
-	hook_addrs = hook_addr #later .append(
+	hook_addrs = []
+	i=0
+	for to_hook in gimme:
+		if not to_hook['enabled']:
+			continue
+		hook_addr = pyd.func_resolve_debuggee(to_hook['dll'], to_hook['func'])
+		if not hook_addr:
+			print "[-] couldn't hook address of "+to_hook['func']+" in "+to_hook['dll']
+			sys.exit(-1)
+		print "[+] hooked "+to_hook['func']+"('"+to_hook['argmap']+"') in "+to_hook['dll'] + " at 0x%08x !" % hook_addr
+		hooks.add(pyd, hook_addr, 9, to_hook['callback'], None) # change me later..
+		hook_addrs.append(hook_addr)
 	return hook_addrs
 
 def go():
@@ -61,4 +108,8 @@ def go():
 	pyd.run()
 
 if __name__ == "__main__":
+	gimme.append({'enabled': True, 'dll' : 'advapi32.dll', 'argmap': 'isddi', 'callback': hook_RegCreateKeyExA, 'func' :'RegCreateKeyExA'})
+	gimme.append({'enabled': True, 'dll' : 'advapi32.dll', 'argmap': 'isddi', 'callback': hook_RegQueryValueExA, 'func' :'RegQueryValueExA'})
+	gimme.append({'enabled': True, 'dll' : 'advapi32.dll', 'argmap': 'isddi', 'callback': hook_RegOpenKeyExA, 'func' :'RegOpenKeyExA'})
+	gimme.append({'enabled': True, 'dll' : 'advapi32.dll', 'argmap': 'isddi', 'callback': hook_RegOpenKeyExA, 'func' :'RegOpenKeyA'})
 	go()
